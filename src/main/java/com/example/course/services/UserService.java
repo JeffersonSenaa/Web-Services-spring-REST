fd7cb7466dbb3570341fc01bb001e8880ad38551
JeffersonSenaa <jeffersonsena12144@gmail.com>
2024-06-13 18:36:02 -0300

    Implementando Tratamento de Exceções nas requisições de Delete, PUT e GET

--- src/main/java/com/example/course/services/UserService.java
+++ src/main/java/com/example/course/services/UserService.java
@@ -4,10 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.course.entities.User;
 import com.example.course.repositories.UserRepository;
+import com.example.course.services.exceptions.DatabaseException;
+import com.example.course.services.exceptions.ResourceNotFoundExceptions;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +27,7 @@ public class UserService {
 		
 		public User findById(Long id) {
 			Optional<User> obj = repository.findById(id);
-			return obj.get();
+			return obj.orElseThrow(() -> new ResourceNotFoundExceptions(id));
 		}
 		
 		public User insert(User user) {
@@ -29,13 +35,23 @@ public class UserService {
 		}
 		
 		public void delete(Long id) {
-			repository.deleteById(id);
+			try {
+				repository.deleteById(id);
+			} catch (EmptyResultDataAccessException e) {
+				throw new ResourceNotFoundExceptions(id);
+			} catch(DataIntegrityViolationException e) {
+				throw new DatabaseException(e.getMessage());
+			}
 		}
 		
 		public User update(Long id, User user) {
-			User entity = repository.getReferenceById(id);
-			updateData(entity, user);
-			return repository.save(entity);
+			try {
+				User entity = repository.getReferenceById(id);
+				updateData(entity, user);
+				return repository.save(entity);
+			} catch (EntityNotFoundException e){
+				throw new ResourceNotFoundExceptions(id);
+			}
 		}
 
 		private void updateData(User entity, User user) {
---
+++ src/main/java/com/example/course/services/exceptions/ResourceNotFoundExceptions.java
@@ -0,0 +1,9 @@
+package com.example.course.services.exceptions;
+
+public class ResourceNotFoundExceptions extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundExceptions(Object id) {
+		super("Resource not found. Id" + id);
+	}
+}
